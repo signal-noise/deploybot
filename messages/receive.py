77eff9c11bbe1b0ec7bfd0f67f620a66e2f8cc77
marcelkornblum <me@marcelkornblum.com>
2018-11-28 15:09:45 +0000

    param parsing

--- messages/receive.py
+++ messages/receive.py
@@ -16,6 +16,9 @@ if logger.handlers:
         logger.removeHandler(handler)
 logging.basicConfig(level=logging.INFO)
 
+def get_form_variable_value(form_var):
+    return form_var[0]
+
 
 def is_request_valid(token, team_id):
     logging.info('validating %s against %s and %s against %s' % (token, os.environ['SLACK_VERIFICATION_TOKEN'], team_id, os.environ['SLACK_TEAM_ID']))
@@ -27,6 +30,10 @@ def is_request_valid(token, team_id):
 
 def receive(event, context):
     data = parse_qs(event['body'])
+    token = get_form_variable_value(data['token'])
+    team_id = get_form_variable_value(data['team_id'])
+    command = get_form_variable_value(data['command'])
+    text = get_form_variable_value(data['text'])
 
     response = {
         "statusCode": 200,
@@ -41,26 +48,24 @@ def receive(event, context):
         }
     }
 
-    if not is_request_valid(data[u'token'], data[u'team_id']):
+    if not is_request_valid(token, team_id):
         logging.error("Authentication Failed")
         response['statusCode'] = 403
         response['body']['attachments'][0]['text'] = "Authentication Failed"
         return
 
-    if 'text' not in data:
+    if len(text) == 0:
         logging.error("No text in command")
         response['statusCode'] = 401
         response['body']['attachments'][0]['text'] = "No text in command - what would you like me to do?"
         return
 
-    if data['command'] == 'cimon':
-        response['body']['text'] = help()
-        response['attachments'] = []
-
-    else:
+    if command != 'cimon':
         logging.error("Unexpected command")
         raise Exception("Unexpected command.")
-
+        
+    response['body']['text'] = help()
+    response['attachments'] = []
 
     return response
 
