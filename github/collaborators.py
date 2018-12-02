80690d7332f89c04083195f15e3d6434f91966c7
marcelkornblum <me@marcelkornblum.com>
2018-12-02 22:29:18 +0000

    invoke chain, repo collaborators

--- github/collaborators.py
+++ github/collaborators.py
@@ -8,6 +8,8 @@ from botocore.vendored import requests
 
 GITHUB_GRAPHQL_URI="https://api.github.com/graphql"
 
+GRAPHQL_QUERY_COLLABORATORS="query {repository(owner:\"%s\", name:\"%s\") { collaborators (first:100) { totalCount nodes { login }}}}"
+
 dynamodb = boto3.resource('dynamodb')
 
 logger = logging.getLogger()
@@ -18,11 +20,16 @@ logging.basicConfig(level=logging.INFO)
 
 
 def send(event, context):
-    data = json.loads(event['body'])
-    if 'repository' not in data:
+    if 'repository' not in event:
         logging.error("Validation Failed")
         raise Exception("Couldn't set up the repository.")
         return
+
+    try:
+        (username, repository) = event['repository'].split('/')
+    except ValueError as e:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't set up the repository.")
 
     headers = {
         'Content-Type': 'application/json',
@@ -30,15 +37,17 @@ def send(event, context):
     }
     uri = 'https://api.github.com/graphql'
     payload = {
-        "query": "query { viewer { login }}",
+        "query": GRAPHQL_QUERY_COLLABORATORS % (username, repository),
     }
 
     r = requests.post(uri, data=json.dumps(payload), headers=headers)
+    data = r.json()
+    logging.info(data)
 
     # create a response
     response = {
         "statusCode": r.status_code,
-        "body": r.text
+        "body": data['data']['repository']
     }
 
     return response
