4aac558faa2cd379d0514b6c275c56c3bdac9929
marcelkornblum <me@marcelkornblum.com>
2018-12-03 22:00:07 +0000

    begin interactive reception

--- slack/interactive.py
+++ slack/interactive.py
@@ -5,6 +5,7 @@ import os
 import boto3
 
 from botocore.vendored import requests
+from urlparse import parse_qs
 
 dynamodb = boto3.resource('dynamodb')
 
@@ -15,28 +16,89 @@ if logger.handlers:
 logging.basicConfig(level=logging.INFO)
 
 
+#
+#
+# Response logic
+#
+#
+
 def receive(event, context):
-    data = json.loads(event['body'])
-    if 'text' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't send the message.")
-        return
+    """
+    Handler for slash interactive events sent from Slack
+    """
+    if not is_request_valid(event):
+        logging.error("Authentication Failed")
+        return response({"message": "Authentication Failed"}, 401)
 
-    headers = {'Content-Type': 'application/json'}
-    uri = os.environ['SLACK_WEBHOOK_URL']
-    channel = os.environ['SLACK_CHANNEL']
-    payload = {
-        "text": data['text'],
-        "channel": channel,
+    d = parse_qs(event['body'])
+
+    data = {}
+    for key, value in d.iteritems():
+        logging.info('got data "%s"="%s"'%(key, value))
+        data[key] = get_form_variable_value(value)
+
+    if 'actions' not in data:
+        logging.error("Unexpected event")
+        return response({"message": "Unexpected event"}, 500)
+
+    context = {
+        "type": data['type'],
+        "channel": data['channel'],
+        "user": data['user'],
+        "team": data['team'],
+        "callback_id": data['callback_id'],
+        "response_url": data['response_url'],
+        "trigger_id": data['trigger_id'],
     }
+        
+    return call_function(data['text'], context)
 
-    r = requests.post(uri, data=json.dumps(payload), headers=headers)
 
-    # create a response
+def response(body, status=200):
+    """
+    Builds the data strcture for an AWS Lambda reply
+    """
     response = {
-        "statusCode": r.status_code,
-        "body": r.text
+        "statusCode": int(status),
+        "isBase64Encoded": False,
+        "headers": { 
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(body)
     }
-
+    logging.info(response)
     return response
 
+
+def is_request_valid(event):
+    """
+    Validates using Signed Secret approach:
+    https://api.slack.com/docs/verifying-requests-from-slack
+    """
+    body = event['body']
+    ts = event['headers']['X-Slack-Request-Timestamp']
+    slack_signature = event['headers']['X-Slack-Signature']
+
+    # Timestamp must be recent
+    now = datetime.now()
+    ts_date = datetime.fromtimestamp(int(ts))
+    if now - timedelta(minutes=5) > ts_date:
+        return False
+
+    basestring = ":".join((SLACK_SIGNING_SECRET_VERSION, ts, body))
+    signature = hmac.new(
+        os.environ['SLACK_SIGNING_SECRET'],
+        basestring,
+        digestmod=hashlib.sha256
+    ).hexdigest()
+    signature = '%s=%s' % (SLACK_SIGNING_SECRET_VERSION, signature)
+    
+    return hmac.compare_digest(str(signature), str(slack_signature))
+
+
+def get_form_variable_value(form_var):
+    """
+    Cleans up submitted form vars on the assumption they are single values
+    """
+    return form_var[0]
