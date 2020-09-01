304a272a627783d7546a0be10f357533f4c0e0d2
Guy <38443772+nattog@users.noreply.github.com>
2020-09-01 10:23:38 +0100

    Adds extra information to slack responses (#59)
    * Adds extra information to slack responses
    
    * Consistent vars naming scheme
    
    * Interface syntax without spacing
    
    * Tidy get response
    
    * new line tidy up
    
    * Reorders input validation and formatting
    
    * Fix typos
    
    * Minor tweaks

--- slack/command.py
+++ slack/command.py
@@ -13,22 +13,30 @@ import boto3
 
 COMMAND = os.environ['COMMAND']
 
+SET_OPTIONS = {
+        "basedomain": "baseurl",
+        "urlpattern": "url_pattern",
+        "urlseparator": "url_separator",
+        "url": "url"
+    }
+
 FN_RESPONSE_HELP = ("There are a few things you can ask me to do. "
                     "Try `%s setup signal-noise/reponame` to get started, "
-                    "or `%s config` to see what's going on in this channel. "
+                    "or `%s get` to see what's going on in this channel. "
                     "There's `%s hello` to connect your Github and Slack accounts, and `%s bye` to undo that. "
-                    "There's also `%s reset` if you want to start over on this channel's configuration." % (COMMAND, COMMAND, COMMAND, COMMAND, COMMAND))
-FN_RESPONSE_SET = ("Call this with two or three arguments; e.g. `%s set baseurl test.com`, or `%s set URL preview preview.test.com`. "
+                    "There's also `%s reset` if you want to start over on this channel's configuration."
+                    "To see information about setting variables, try `%s set`" % (COMMAND, COMMAND, COMMAND, COMMAND, COMMAND, COMMAND))
+FN_RESPONSE_SET = ("Call this with two or three arguments; e.g. `%s set basedomain test.com`, or `%s set URL preview preview.test.com`. "
                    "Settings you can set here are: \n"
                    "- `url`: A domain specific to an environment, to override the *url_pattern* rule. You must call this with the environment name and then the FQDN as above. Please don't include protocol but DO ensure HTTPS is supported.\n"
-                   "- `url_pattern`: The pattern used to generate URLs for environments. Defaults to `https://{environment}{url_separator}{baseurl}/`. Please include all those 3 variables in that format.\n"
-                   "- `baseurl`: Actually a domain: if your PR environment is at `https://pr27.test.com`, this is `test.com`. This will be used to create all environment URLs not explicitly specified.\n"
-                   "- `url_separator`: The charater used to join your specific environment name to the baseurl. I.e. in `https://pr27.test.com`, the `.` between `pr27` and `test.com`. Defaults to `.` " % (COMMAND, COMMAND))
+                   "- `urlpattern`: The pattern used to generate URLs for environments. Defaults to `https://{environment}{urlseparator}{basedomain}/`. Please include all those 3 variables in that format.\n"
+                   "- `basedomain`: The domain: if your PR environment is at `https://pr27.test.com`, this is `test.com`. This will be used to create all environment URLs not explicitly specified.\n"
+                   "- `urlseparator`: The character used to join your specific environment name to the basedomain. I.e. in `https://pr27.test.com`, the `.` between `pr27` and `test.com`. Defaults to `.` " % (COMMAND, COMMAND))
 FN_RESPONSE_SET_CONFIRM = "Great, I've set %s to %s."
-FN_RESPONSE_UNSET = "Call this with the one or two arguments you called `set` with, and no value part - e.g. `%s unset baseurl`" % COMMAND
-FN_RESPONSE_CONFIG_EXISTS = "This channel is currently set up for `%s`. Some GitHub users may not be connected."
-FN_RESPONSE_CONFIG_NOTEXISTS = "This channel hasn't got any configuration at the moment."
-FN_RESPONSE_CONFIG_ALL_GH_KNOWN = "I know all the users oon this repository"
+FN_RESPONSE_UNSET = "Call this with the one or two arguments you called `set` with, and no value part - e.g. `%s unset basedomain`" % COMMAND
+FN_RESPONSE_GET_EXISTS = "This channel is currently set up for `%s` \nbasedomain: `%s` \n url: `%s` \n urlpattern: `%s` \n urlseparator: `%s` \n Some GitHub users may not be connected."
+FN_RESPONSE_GET_NOTEXISTS = "This channel hasn't got any configuration at the moment."
+FN_RESPONSE_GET_ALL_GH_KNOWN = "I know all the users on this repository"
 FN_RESPONSE_SETUP = "Setting up `%s` in this channel. Note that you won't be able to use this channel for another project, or use that repo in another channel. You should run `set` to get your environment URLs configured."
 FN_RESPONSE_RESET_SUCCESS = "Removed this channel's configuration for `%s`"
 FN_RESPONSE_HELLO = "Hi! Who are you when you're not here?"
@@ -42,7 +50,7 @@ ERR_SET = "Something went wrong. Have you set this channel up with a repo yet?"
 ERR_SET_SETTING_NOT_RECOGNISED = "I didn't recognise that setting. "
 ERR_SET_SETTING_2_ARGS = "That setting needs a single value; unset with just the setting name. "
 ERR_SET_SETTING_3_ARGS = "That setting needs two values; unset with the setting name and the first argument. "
-ERR_SET_URLPATTERN_SPECIFIC_VARS = "You need to include all of the following variables in your pattern: `{environment}`, `{url_separator}`, `{baseurl}`"
+ERR_SET_URLPATTERN_SPECIFIC_VARS = "You need to include all of the following variables in your pattern: `{environment}`, `{urlseparator}`, `{basedomain}`"
 ERR_SETUP_PARAM_MISSING = "You need to tell me which repo. Try `%s setup username/reponame`" % COMMAND
 ERR_SETUP_PARAM_FORMAT = ("The repo name should be two parts; the GitHub username and the actual"
                           " repository name. e.g. the React library's repo would be `facebook/react`")
@@ -83,7 +91,6 @@ def help(*args, **kwargs):
     """
     return slack_response(FN_RESPONSE_HELP)
 
-
 def set(text, context):
     """
     Allows individual settings to be created
@@ -91,20 +98,7 @@ def set(text, context):
     if len(text) == 0 or text.strip() == '':
         return slack_response(FN_RESPONSE_SET)
 
-    parts = text.split()
-    setting = parts[0].lower()
-
-    if setting not in (
-        'url',
-        'baseurl',
-        'url_pattern',
-        'urlpattern',
-        'url-pattern',
-        'url_separator',
-        'urlseparator',
-        'url-separator',
-    ):
-        return slack_response(ERR_SET_SETTING_NOT_RECOGNISED + FN_RESPONSE_SET)
+    parts, setting = validate_input(text, FN_RESPONSE_SET)
 
     if setting == 'url':
         if len(parts) != 3:
@@ -114,14 +108,13 @@ def set(text, context):
         }
         val_type = "M"
 
-    if setting in ('urlpattern', 'url-pattern', 'url_pattern', ):
-        setting = 'url_pattern'
+    if setting == 'url_pattern':
         if len(parts) != 2:
             return slack_response(ERR_SET_SETTING_2_ARGS + FN_RESPONSE_SET)
         value = parts[1]
         if ('{environment}' not in value and
-            '{url_separator}' not in value and
-                '{baseurl}' not in value):
+            '{urlseparator}' not in value and
+                '{basedomain}' not in value):
             return slack_response(ERR_SET_URLPATTERN_SPECIFIC_VARS)
         val_type = "S"
 
@@ -131,8 +124,7 @@ def set(text, context):
         value = parts[1]
         val_type = "S"
 
-    if setting in ('urlseparator', 'url-separator', 'url_separator', ):
-        setting = 'url_separator'
+    if setting == 'url_separator':
         if len(parts) != 2:
             return slack_response(ERR_SET_SETTING_2_ARGS + FN_RESPONSE_SET)
         value = parts[1]
@@ -173,34 +165,19 @@ def unset(text, context):
     if text is None or len(text) == 0:
         return slack_response(FN_RESPONSE_UNSET)
 
-    parts = text.split()
-    setting = parts[0].lower()
-
-    if setting not in (
-        'url',
-        'baseurl',
-        'url_pattern',
-        'urlpattern',
-        'url-pattern',
-        'url_separator',
-        'urlseparator',
-        'url-separator',
-    ):
-        return slack_response(ERR_SET_SETTING_NOT_RECOGNISED + FN_RESPONSE_UNSET)
+    parts, setting = validate_input(text, FN_RESPONSE_UNSET)
 
     if setting == 'url':
         if len(parts) != 2:
             return slack_response(ERR_SET_SETTING_3_ARGS + FN_RESPONSE_UNSET)
         val_type = "M"
 
-    if setting in ('urlpattern', 'url-pattern', 'url_pattern'):
-        setting = 'url_pattern'
+    if setting == 'url_pattern':
         if len(parts) != 1:
             return slack_response(ERR_SET_SETTING_2_ARGS + FN_RESPONSE_UNSET)
         val_type = "S"
 
-    if setting in ('urlseparator', 'url-separator', 'url_separator'):
-        setting = 'url_separator'
+    if setting == 'url_separator':
         if len(parts) != 1:
             return slack_response(ERR_SET_SETTING_2_ARGS + FN_RESPONSE_UNSET)
         val_type = "S"
@@ -243,7 +220,7 @@ def unset(text, context):
     return slack_response(FN_RESPONSE_UNSET)
 
 
-def config(text, context):
+def get(text, context):
     """
     Prints out current setup information
     """
@@ -251,8 +228,9 @@ def config(text, context):
     entries = table.scan()
     for entry in entries['Items']:
         if entry['slack_channelid'] == context['channel_id']:
-            return slack_response(FN_RESPONSE_CONFIG_EXISTS % entry['repository'])
-    return connect_github_user(FN_RESPONSE_CONFIG_NOTEXISTS, entry['repository'], entry['slack_channelid'], FN_RESPONSE_CONFIG_ALL_GH_KNOWN)
+            return slack_response(FN_RESPONSE_GET_EXISTS % (entry['repository'], entry['setting_baseurl'], entry['setting_url'], entry['setting_url_pattern'], entry['setting_url_separator']))
+    
+    return connect_github_user(FN_RESPONSE_GET_NOTEXISTS, entry['repository'], entry['slack_channelid'], FN_RESPONSE_GET_ALL_GH_KNOWN)
 
 
 def setup(repo=None, context=None):
@@ -357,7 +335,7 @@ def bye(text, context):
 
 def deploy(text, context):
     """
-    Manually trigegrs a deployment to a specific environment
+    Manually triggers a deployment to a specific environment
     """
     parts = text.split()
     if len(parts) != 3:
@@ -570,7 +548,7 @@ def receive(event, context):
 
 def response(body, status=200):
     """
-    Builds the data strcture for an AWS Lambda reply
+    Builds the data structure for an AWS Lambda reply
     """
     response = {
         "statusCode": int(status),
@@ -600,6 +578,26 @@ def call_function(command_text, context):
     except Exception as e:
         logging.error(e)
         return slack_response(ERR_NO_FUNC_FOUND)
+
+
+def validate_input(text, command_response):
+    """
+    Validates user input and formats it for processing
+    """
+    parts = text.split()
+    setting = format_input(parts[0].lower())
+
+    if setting not in SET_OPTIONS.keys():
+        return slack_response(ERR_SET_SETTING_NOT_RECOGNISED + command_response)
+
+    return (parts, setting)
+
+
+def format_input(setting):
+    """
+    Formats valid input setting to db keyword
+    """
+    return SET_OPTIONS.get(setting, None)
 
 
 def get_form_variable_value(form_var):
